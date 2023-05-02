5c07438d643163dec742bbc2183d2e41cd13c0c0
Jon Pugh <jon@thinkdrop.net>
2023-05-02 15:36:14 -0400

    Append vardot support settings include to sites/default/settings.php

---
+++ settings/settings.vardot.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * settings.vardot.php
+ *
+ * This file will be included by any site using drupal/vardot_support, if it has been properly appended to settings.php by drupal-scaffolding.
+ *
+ * This is an attempt at standardizing settings.php for Vardot Supported sites.
+ *
+ */
+
