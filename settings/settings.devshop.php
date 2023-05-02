eb7a0d1276313e5cd21f17feb69ee1c48709aba8
Jon Pugh <jon@thinkdrop.net>
2023-05-02 17:18:26 -0400

    Adding settings.devshop.php to drupal/vardot_support scaffolding.

---
+++ settings/settings.devshop.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file settings.devsohp.php
+ *
+ * This file is included by the file sites/X.com/settings.php that is written by devshop.
+ *
+ * This include is added so that devshop reads the globally used sites/default/settings.php.
+ */
+include "settings.php";
