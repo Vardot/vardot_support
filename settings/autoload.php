204f073758582ff50857c3c3b26e2967d0b5f9f6
Jon Pugh <jon@thinkdrop.net>
2023-05-02 17:46:11 -0400

    Adding comments and IF statement to the vardot support autoload include.

--- settings/autoload.php
+++ settings/autoload.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * @file autoload.php
+ *
+ * This file is included very early in calls to this site.
+ *
+ * This allows us to alter things like DRUSH_OPTIONS_URI before drush bootstraps.
+ */
 
-$lando_info = json_decode(getenv('LANDO_INFO'), TRUE);
-putenv('DRUSH_OPTIONS_URI=' . $lando_info['appserver']['urls'][1]);
+/**
+ * Set DRUSH_OPTIONS_URI from lando info, if it exists.
+ */
+if ((bool) getenv('LANDO')) {
+  $lando_info = json_decode(getenv('LANDO_INFO'), TRUE);
+  putenv('DRUSH_OPTIONS_URI=' . $lando_info['appserver']['urls'][1]);
+}
