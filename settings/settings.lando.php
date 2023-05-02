b3463242992300f1bfe46e390339ab0501f3f899
Jon Pugh <jon@thinkdrop.net>
2023-05-02 17:00:55 -0400

    Set lando database connection via LANDO_INFO env var.

--- settings/settings.lando.php
+++ settings/settings.lando.php
@@ -4,15 +4,27 @@
  * Lando Local Development Settings.
  */
 
+$database_host = 'database';
+$lando_info = json_decode(getenv('LANDO_INFO'), TRUE);
+
 $databases['default']['default'] = [
   'driver' => 'mysql',
-  'database' => getenv('DB_NAME'),
-  'username' => getenv('DB_USER'),
-  'password' => getenv('DB_PASSWORD'),
-  'host' => getenv('DB_HOST'),
-  'port' => getenv('DB_PORT'),
+  'database' => $lando_info[$database_host]['creds']['database'],
+  'username' => $lando_info[$database_host]['creds']['user'],
+  'password' => $lando_info[$database_host]['creds']['password'],
+  'host' => $database_host,
+  'port' => '3306',
 ];
 
 $settings['hash_salt'] = md5(getenv('LANDO_HOST_IP'));
+
+# See https://www.drupal.org/docs/getting-started/installing-drupal/trusted-host-settings
+$settings['trusted_host_patterns'] = [
+  # Lando Proxy
+  '^'.getenv('LANDO_APP_NAME').'\.lndo\.site$',      # lando proxy access
+
+  # Lando Share
+  '^'.getenv('LANDO_APP_NAME').'\.localtunnel\.me$', # lando share access
+];
 
 // @TODO: Enable development stuff here.
