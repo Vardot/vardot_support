4e5968dfcb20dcd822a4336786cf865d38ef7321
Jon Pugh <jon@thinkdrop.net>
2023-05-11 13:40:26 -0400

    Add vardot settings.php file.

---
+++ settings/settings.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file settings.php
+ *
+ * This is vardot_support's default settings.php file.
+ * It can replace sites/default/settings.php.
+ *
+ * Copy this file into place, or you can use a symlink.
+ *
+ */
+
+/**
+ * Added by drupal/vardot_support.
+ *
+ * The file below includes acquia/blt settings, Lando config, and other things globally.
+ */
+require DRUPAL_ROOT . "/modules/contrib/vardot_support/settings/settings.vardot.php";
+
