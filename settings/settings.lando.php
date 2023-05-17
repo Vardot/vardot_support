27db3b052d87961ada0c3829d426cc354d0e52b6
Jon Pugh <jon@thinkdrop.net>
2023-05-17 11:22:51 -0400

    Allow sites to override lando database host and automatically look for multisite databases.

--- settings/settings.lando.php
+++ settings/settings.lando.php
@@ -4,18 +4,30 @@
  * Lando Local Development Settings.
  */
 
-$database_host = 'database';
 $lando_info = json_decode(getenv('LANDO_INFO'), TRUE);
+
+// If $lando_database_host was not set in site's settings.php file, detect multisite or use default.
+if (empty($lando_database_host)) {
+
+  # If using a multisite $site_path, and a lando database exists by that name, use it.
+  list($s, $site_slug) = explode('/', $site_path);
+  if (!isset($lando_database_host) && $site_path != 'default' && isset($lando_info[$site_slug]['creds']['database'])) {
+    $lando_database_host = $site_slug;
+  }
+  else {
+    // If no $lando_database_host, use "database".
+    $lando_database_host = 'database';
+  }
+}
 
 $databases['default']['default'] = [
   'driver' => 'mysql',
-  'database' => $lando_info[$database_host]['creds']['database'],
-  'username' => $lando_info[$database_host]['creds']['user'],
-  'password' => $lando_info[$database_host]['creds']['password'],
-  'host' => $database_host,
+  'database' => $lando_info[$lando_database_host]['creds']['database'],
+  'username' => $lando_info[$lando_database_host]['creds']['user'],
+  'password' => $lando_info[$lando_database_host]['creds']['password'],
+  'host' => $lando_database_host,
   'port' => '3306',
 ];
-
 $settings['hash_salt'] = md5(getenv('LANDO_HOST_IP'));
 
 # See https://www.drupal.org/docs/getting-started/installing-drupal/trusted-host-settings
