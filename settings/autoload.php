b13f6f8d0f8831bbd54978f3c48c8a22bd3e8a50
Jon Pugh <jon@thinkdrop.net>
2023-05-22 12:41:12 -0400

    Automatically detect LANDO main URL based on LANDO_APP_NAME.

--- settings/autoload.php
+++ settings/autoload.php
@@ -10,7 +10,17 @@
 /**
  * Set DRUSH_OPTIONS_URI from lando info, if it exists.
  */
-if ((bool) getenv('LANDO')) {
+if ((bool) getenv('LANDO') && empty(getenv('DRUSH_OPTIONS_URI'))) {
   $lando_info = json_decode(getenv('LANDO_INFO'), TRUE);
-  putenv('DRUSH_OPTIONS_URI=' . $lando_info['appserver']['urls'][1]);
+
+  // Set DRUSH_OPTIONS_URI unless already set.
+  foreach ($lando_info['appserver']['urls'] as $url) {
+    if (strpos($url, 'https') === 0) {
+      if (str_contains($url, getenv('LANDO_APP_NAME'))) {
+        $main_url = $url;
+      }
+    }
+  }
+  putenv('DRUSH_OPTIONS_URI=' . $main_url);
+
 }
