82da3445358b66e312087234246c4a68af4b7bc6
Jon Pugh <jon@thinkdrop.net>
2023-06-14 13:55:14 -0400

    Set standard env vars from lando info.

--- settings/settings.lando.php
+++ settings/settings.lando.php
@@ -5,22 +5,6 @@
  */
 
 $lando_info = json_decode(getenv('LANDO_INFO'), TRUE);
-
-// If you have a special lando setup, such as multisite with multiple databases, add logic to your settings.php file
-// before the vardot_support include to set $lando_database_host to be the lando service name.
-if (empty($lando_database_host)) {
-    // Default to "database".
-    $lando_database_host = 'database';
-}
-
-$databases['default']['default'] = [
-  'driver' => 'mysql',
-  'database' => $lando_info[$lando_database_host]['creds']['database'],
-  'username' => $lando_info[$lando_database_host]['creds']['user'],
-  'password' => $lando_info[$lando_database_host]['creds']['password'],
-  'host' => $lando_info[$lando_database_host]['internal_connection']['host'],
-  'port' => $lando_info[$lando_database_host]['internal_connection']['port'],
-];
 $settings['hash_salt'] = md5(getenv('LANDO_HOST_IP'));
 
 # See https://www.drupal.org/docs/getting-started/installing-drupal/trusted-host-settings
@@ -44,3 +28,13 @@ if (!(bool) getenv('LANDO_PROD_MODE')) {
 
 // Do not redirect to www if using httpswww module.
 $config['httpswww.settings']['prefix'] = 'no';
+
+// To alter what database host is used, set LANDO_DATABASE_HOST.
+$lando_database_host = getenv('LANDO_DATABASE_HOST') ?: 'database';
+
+// Set 'standard' env vars.
+putenv('MYSQL_DATABASE=' . $lando_info[$lando_database_host]['creds']['database']);
+putenv('MYSQL_USER=' . $lando_info[$lando_database_host]['creds']['user']);
+putenv('MYSQL_PASSWORD=' . $lando_info[$lando_database_host]['creds']['password']);
+putenv('MYSQL_HOSTNAME=' . $lando_info[$lando_database_host]['internal_connection']['host']);
+putenv('MYSQL_PORT=' . $lando_info[$lando_database_host]['internal_connection']['port']);
